d7d51830fcb734a832e6c9336dd5ec893a76cdc2
aocbn <ledinhan.fit@gmail.com>
2016-01-12 16:50:59 +0700

    commit exercise

--- app/src/main/java/com/alanddev/gymlover/ui/WorkoutActivity.java
+++ app/src/main/java/com/alanddev/gymlover/ui/WorkoutActivity.java
@@ -59,8 +59,8 @@ public class WorkoutActivity extends AppCompatActivity {
         getSupportActionBar().setTitle(getResources().getString(R.string.title_activity_workout));
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
