2f48dd6ef41792c19aac73b86fa0d0064e88ad94
Long Tran Dac <longtdbk@gmail.com>
2016-01-18 17:33:22 +0700

    Report Init (continue)

--- app/src/main/java/com/alanddev/gymlover/ui/ReportActivity.java
+++ app/src/main/java/com/alanddev/gymlover/ui/ReportActivity.java
@@ -1,5 +1,8 @@
 package com.alanddev.gymlover.ui;
 
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,16 +15,21 @@ import android.view.View;
 import com.alanddev.gymlover.R;
 import com.alanddev.gymlover.util.Utils;
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
+
 
 import java.util.ArrayList;
 
 public class ReportActivity extends AppCompatActivity {
 
-    BarChart chart;
+    LineChart chart;
     String year;
 
     @Override
@@ -60,31 +68,51 @@ public class ReportActivity extends AppCompatActivity {
     }
 
     public void getData(){
-        ArrayList<String> xAxis = new ArrayList<>();
-        chart = (BarChart) findViewById(R.id.chart);
-
-        for (int m = 1; m <=12; m++){
-            xAxis.add(Integer.toString(m));
+        chart = (LineChart)findViewById(R.id.chart);
+        int count = 15;
+        int range = 20;
+        ArrayList<String> xVals = new ArrayList<String>();
+        for (int i = 0; i < count; i++) {
+            xVals.add((i) + "");
         }
 
-        ArrayList<BarDataSet> dataSets = null;
-        ArrayList<BarEntry> valueSetY = new ArrayList<>();
-//        ArrayList<Trend> trends = transactionController.getAmountTrendByMonths(option, Utils.getWallet_id(), monthBegin, monthEnd, year);
-//        listView.setAdapter(new TrendAdapter(this, trends));
-        for (int i = 0; i<12;i++){
-            valueSetY.add(new BarEntry(i,i));
-        }
-        year  = Utils.getYear();
-        dataSets = new ArrayList<>();
-        BarDataSet barDataSet1 = new BarDataSet(valueSetY, year);
-        barDataSet1.setColors(ColorTemplate.COLORFUL_COLORS);
-        dataSets.add(barDataSet1);
+        ArrayList<Entry> yVals = new ArrayList<Entry>();
 
-        BarData data = new BarData(xAxis,dataSets);
+        for (int i = 0; i < count; i++) {
+
+            float mult = (range + 1);
+            float val = (float) (Math.random() * mult) + 3;// + (float)
+            yVals.add(new Entry(val, i));
+        }
+
+        // create a dataset and give it a type
+        LineDataSet set1 = new LineDataSet(yVals, "DataSet 1");
+        // set1.setFillAlpha(110);
+        // set1.setFillColor(Color.RED);
+
+        // set the line to be drawn like this "- - - - - -"
+        set1.enableDashedLine(10f, 5f, 0f);
+        set1.enableDashedHighlightLine(10f, 5f, 0f);
+        set1.setColor(Color.BLACK);
+        set1.setCircleColor(Color.BLACK);
+        set1.setLineWidth(1f);
+        //set1.setCircleRadius(3f);
+        set1.setDrawCircleHole(false);
+        set1.setValueTextSize(9f);
+        set1.setFillAlpha(65);
+        set1.setFillColor(Color.BLACK);
+        set1.setDrawFilled(true);
+//         set1.setS(new LinearGradient(0, 0, 0, chart.getHeight(),
+//         Color.BLACK, Color.WHITE, Shader.TileMode.MIRROR));
+
+        ArrayList<LineDataSet> dataSets = new ArrayList<LineDataSet>();
+        dataSets.add(set1); // add the datasets
+
+        // create a data object with the datasets
+        LineData data = new LineData(xVals, dataSets);
+
+        // set data
         chart.setData(data);
-        chart.setDescription("");
-        chart.animateXY(2000, 2000);
-        chart.invalidate();
 
 
     }
