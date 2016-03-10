213fb59332dc3dd8c26e6b06290e36a36f59d467
aocbn <ledinhan.fit@gmail.com>
2016-03-10 23:17:43 +0700

    commit workout

--- app/src/main/java/com/alanddev/gymlover/ui/WorkoutActivity.java
+++ app/src/main/java/com/alanddev/gymlover/ui/WorkoutActivity.java
@@ -77,7 +77,7 @@ public class WorkoutActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_workout, menu);
+        getMenuInflater().inflate(R.menu.menu_category, menu);
         return true;
     }
 
--- app/src/main/java/com/alanddev/gymlover/ui/WorkoutRunActivity.java
+++ app/src/main/java/com/alanddev/gymlover/ui/WorkoutRunActivity.java
@@ -342,7 +342,7 @@ public class WorkoutRunActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_workout_run, menu);
+        getMenuInflater().inflate(R.menu.menu_category, menu);
         return true;
     }
 
